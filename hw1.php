9e919a91e96daf9310ff41f817509e00d5b74f23
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-10-09 22:24:59 -0700

    first commit
    hw1, hw2
    xampp, portal page and layout

---
+++ hw1.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <title>IT261 Portal</title>
+        <meta name="author" content="K. Abell" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,inital-scale=1" />
+        <meta name="robots" content="noindex,nofollow" />
+        <link href="css/reset.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+    
+        <div class="container">
+
+            <header>
+
+                <div class="inner">
+                    <h1 class="title"><a href="index.php">IT261 Portal Page</a></h1>
+                </div>                
+
+            </header>
+
+            <nav>
+
+                <div class="inner">
+
+                    <ul>
+                        <li><a href="hw1.php">XAMPP</a></li>
+                        <li><a href="#">Using a Switch</a></li>
+                        <li><a href="#">Troubleshooting</a></li>
+                        <li><a href="#">Calculating Form</a></li>
+                        <li><a href="#">Email</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Database</a></li>
+                    </ul>
+
+                </div>
+
+            </nav>
+
+            <div class="content inner sidebar">
+
+                <main>
+
+                    <h1>MAMP - HW1</h1>
+
+                    <p>
+                        I am using XAMPP :).
+                    </p>
+
+                    <img src="hw1/hw1-xampp-screen.jpg" alt="xampp up and running" />
+
+                    <img src="hw1/hw1-php-error.jpg" alt="php errors" />
+
+                    <a href="hw1/test_xampp.php">Click to see php errors!</a>
+
+                </main>
+
+                <aside>
+
+                    <h2>Headline Two</h2>
+
+                    <p>
+                        Eget egestas purus viverra accumsan in nisl nisi scelerisque. Egestas integer eget aliquet nibh praesent tristique magna sit amet. Aliquet nec ullamcorper sit amet. Odio facilisis mauris sit amet. Aliquam purus sit amet luctus venenatis.
+                    </p>
+
+                    <p>
+                        Ultricies mi quis hendrerit dolor magna eget est lorem. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Arcu cursus euismod quis viverra nibh cras. Amet justo donec enim diam vulputate ut pharetra. Molestie nunc non blandit massa enim nec dui. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Ultrices dui sapien eget mi proin.
+                    </p>
+
+                </aside>
+
+            </div> <!-- end content -->
+
+            <footer>
+
+                <div class="inner">
+                    <div>
+                        &copy; <?php echo date('Y'); ?>, Kira Abell. All Rights Reserved.
+                    </div>
+    
+                    <div>
+                        <a href="https://validator.w3.org/check/referer">HTML5</a> | 
+                        <a href="https://jigsaw.w3.org/css-validator/check?uri=referer">CSS3</a> | 
+                        <a href="https://wave.webaim.org/refer">WAVE</a>
+                    </div>
+                </div>
+
+            </footer>
+
+        </div> <!-- end container -->    
+    
+    </body>
+    
+</html>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <title>IT261 Portal</title>
+        <meta name="author" content="K. Abell" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,inital-scale=1" />
+        <meta name="robots" content="noindex,nofollow" />
+        <link href="css/reset.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+    
+        <div class="container">
+
+            <header>
+
+                <div class="inner">
+                    <h1 class="title"><a href="index.php">IT261 Portal Page</a></h1>
+                </div>                
+
+            </header>
+
+            <nav>
+
+                <div class="inner">
+
+                    <ul>
+                        <li><a href="hw1.php">XAMPP</a></li>
+                        <li><a href="#">Using a Switch</a></li>
+                        <li><a href="#">Troubleshooting</a></li>
+                        <li><a href="#">Calculating Form</a></li>
+                        <li><a href="#">Email</a></li>
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Database</a></li>
+                    </ul>
+
+                </div>
+
+            </nav>
+
+            <div class="content inner sidebar">
+
+                <main>
+
+                    <h1>Hello!</h1>
+
+                    <img class="self" src="img/me-500.jpg" alt="me" />
+
+                    <p>
+                        I've traveled over <a href="#">four-thousand miles</a> to get to where I am. Born and raised in Miami, Florida, in 2008, I undertook a two-month car journey to Seattle, Washington, taking in the various sights and sites along the way. To this day, it remains my greatest adventure.
+                    </p>
+
+                    <p>
+                        Now, I'm on a different sort of journey, one of self-improvement as I pursue a degree in the field of <a href="www.hi.com">web development</a>. I have an interest in working with non-profit organizations, which is what I ultimately hope to do with my degree and the skills I develop while working toward it. In the 21st century, it is important for organizations of all sorts to have a viable web presence; this is not limited only to for-profit businesses; non-profits need to have it as well in order to spread awareness of their cause, maximize reach, and obtain the donations they need to continue their good and important work.
+                    </p>
+
+                </main>
+
+                <aside>
+
+                    <h2>Headline Two</h2>
+
+                    <p>
+                        Eget egestas purus viverra accumsan in nisl nisi scelerisque. Egestas integer eget aliquet nibh praesent tristique magna sit amet. Aliquet nec ullamcorper sit amet. Odio facilisis mauris sit amet. Aliquam purus sit amet luctus venenatis.
+                    </p>
+
+                    <p>
+                        Ultricies mi quis hendrerit dolor magna eget est lorem. Mauris in aliquam sem fringilla ut morbi tincidunt augue. Arcu cursus euismod quis viverra nibh cras. Amet justo donec enim diam vulputate ut pharetra. Molestie nunc non blandit massa enim nec dui. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum. Ultrices dui sapien eget mi proin.
+                    </p>
+
+                </aside>
+
+            </div> <!-- end content -->
+
+            <footer>
+
+                <div class="inner">
+                    <div>
+                        &copy; <?php echo date('Y'); ?>, Kira Abell. All Rights Reserved.
+                    </div>
+    
+                    <div>
+                        <a href="https://validator.w3.org/check/referer">HTML5</a> | 
+                        <a href="https://jigsaw.w3.org/css-validator/check?uri=referer">CSS3</a> | 
+                        <a href="https://wave.webaim.org/refer">WAVE</a>
+                    </div>
+                </div>
+
+            </footer>
+
+        </div> <!-- end container -->    
+    
+    </body>
+    
+</html>
