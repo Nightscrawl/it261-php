74074df221c3120af1a5c85a9f16da37b4605ae4
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-10-16 13:50:57 -0700

    site1

---
+++ site1/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <title>IT261 | Site 1</title>
+        <meta name="author" content="K. Abell" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width,inital-scale=1" />
+        <meta name="robots" content="noindex,nofollow" />
+        <link href="css/reset.css" rel="stylesheet" />
+        <link href="css/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+    
+        <div class="container">
+
+            <header>
+
+                <div class="inner">
+                    <h1 class="title"><a href="index.php">IT261 | Site 1</a></h1>
+                </div>                
+
+            </header>
+
+            <nav>
+
+                <div class="inner">
+
+                    <ul>
+                        <li><a href="../">Back to Portal</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Daily</a></li>
+                        <li><a href="#">Customers</a></li>                        
+                        <li><a href="#">Gallery</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+
+                </div>
+
+            </nav>
+
+            <div class="content inner sidebar">
+
+                <main>
+
+                    <h1>Headline One</h1>
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sagittis eu volutpat odio facilisis mauris sit amet massa vitae. Dolor sed viverra ipsum nunc aliquet. Cursus euismod quis viverra nibh cras. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus et netus.
+                    </p>
+
+                    <p>
+                        Iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui. Scelerisque eu ultrices vitae auctor eu augue ut lectus. Ultrices eros in cursus turpis massa tincidunt dui ut. Tellus at urna condimentum mattis pellentesque id nibh tortor. Id semper risus in hendrerit gravida rutrum quisque.
+                    </p>
+
+                    <p>
+                        Enim facilisis gravida neque convallis a cras. Posuere urna nec tincidunt praesent semper feugiat. Amet mattis vulputate enim nulla aliquet porttitor. Ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget. Nibh nisl condimentum id venenatis a condimentum vitae. Facilisi morbi tempus iaculis urna id volutpat lacus. Erat imperdiet sed euismod nisi porta lorem mollis aliquam ut. Sodales ut etiam sit amet nisl purus in mollis. Magna fermentum iaculis eu non diam phasellus vestibulum lorem sed.
+                    </p>
+
+                </main>
+
+                <aside>
+
+                    <h2>Headline Two</h2>
+
+                    <p>
+                        Nibh sed pulvinar proin gravida hendrerit lectus a. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada. Eget mauris pharetra et ultrices neque ornare aenean euismod elementum.
+                    </p>
+
+                    <p>
+                        Consectetur lorem donec massa sapien faucibus et molestie. Consectetur lorem donec massa sapien faucibus et. Quis risus sed vulputate odio ut enim blandit. Hendrerit gravida rutrum quisque non tellus. Donec ac odio tempor orci dapibus ultrices in iaculis. Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus.
+                    </p>
+
+                </aside>
+
+            </div> <!-- end content -->
+
+            <footer>
+
+                <div class="inner">
+                    <div>
+                        &copy; <?php echo date('Y'); ?>, Kira Abell. All Rights Reserved.
+                    </div>
+    
+                    <div>
+                        <a href="https://validator.w3.org/check/referer">HTML5</a> | 
+                        <a href="https://jigsaw.w3.org/css-validator/check?uri=referer">CSS3</a> | 
+                        <a href="https://wave.webaim.org/refer">WAVE</a>
+                    </div>
+                </div>
+
+            </footer>
+
+        </div> <!-- end container -->    
+    
+    </body>
+    
+</html>
