afb0d6b0e82abe30626957747531374470c35083
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-11-23 02:18:25 -0800

    wk8
    fixes wk7 gallery link
    adds wk8 exercises

--- website/config.php
+++ website/config.php
@@ -1,5 +1,11 @@
 <?php
 
+// ob_start();  // prevents header errors before reading the whole page
+
+// define('DEBUG', 'TRUE');  // shows errors
+
+// include('creds.php');
+
 define( 'THIS_PAGE', basename($_SERVER['PHP_SELF']) );  // the page that i'm on is the page that i'm on
 
 
@@ -267,3 +273,19 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {  // IF server has a request method of
         } // end isset
 
 } // close server request method
+
+
+
+
+
+// placed at the very bottom of the config file
+// function myerror($myFile, $myLine, $errorMsg) {
+//     if( defined('DEBUG') && DEBUG ) {
+//         echo 'Error in file: <b>' .$myFile. '</b> on line: <b>' .$myLine. '</b>';
+//         echo 'Error message: <b>' .$errorMsg. '</b>';
+//         die();
+//     } else {
+//         echo 'Houston, we have a problem!';
+//         die();
+//     }
+// }
---
+++ wk8/creds.php
@@ -0,0 +1,8 @@
+<?php
+// credentials for our database
+
+
+define('DB_NAME', 'it261_wk7');
+define('DB_USER', 'kira_db');
+define('DB_PASSWORD', 'judahtrev');
+define('DB_HOST', 'mysql.nightscrawl.net');
