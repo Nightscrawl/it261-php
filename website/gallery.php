2ce52b7879f2ddb5865f9c2ed48e06da04f702a8
Nightscrawl <52441295+Nightscrawl@users.noreply.github.com>
2020-11-23 07:29:01 -0800

    wk8 for loop edits

--- website/gallery.php
+++ website/gallery.php
@@ -19,8 +19,10 @@ include('includes/header.php');
 
         <?php foreach($gallery_albums as $name => $image) : ?>
 
-            <?php $num = array_search( $name, array_keys($gallery_albums) );
-                  $num++; ?>
+            <?php
+                $num = array_search( $name, array_keys($gallery_albums) );
+                $num++;
+            ?>
 
             <tr>
                 <td><img src="img/<?php echo substr($image, 0, 3).$num; ?>.jpg" /></td>
@@ -42,10 +44,6 @@ include('includes/header.php');
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer malesuada nunc vel risus commodo viverra. A diam sollicitudin tempor id eu nisl nunc mi ipsum.</p>
 
     </aside>
-
     
-
-
-
 
 <?php include('includes/footer.php'); ?>   
